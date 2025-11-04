644577eef3b23c9dea460fe7757c5382f9bea5be
Юрий Гапонов <GREATLORDorel@yandex.ru>
2025-11-04 03:10:34 +0300

    создан временный обработчик /download-calendar, возвращающий год. введенный в форму скачивания календаря

--- app/routes/calendar.py
+++ app/routes/calendar.py
@@ -1,6 +1,6 @@
 """Маршрут /calendar для отображения календарного интерфейса."""
 
-from flask import render_template
+from flask import request, render_template
 from .. import app
 
 
@@ -18,3 +18,17 @@ def calendar():
     years = app.config['YEARS']
 
     return render_template('calendar/get_calendar.html', years=years)
+
+
+@app.route('/download-calendar', methods=['POST'])
+def download_calendar():
+    selected_year = request.form.get('year', type=int)
+    app.logger.info(
+        f'На эндпоинт /download-calendar передано значение года'
+        f' для скачивания календаря: {selected_year}'
+    )
+
+    if not selected_year or selected_year not in app.config['YEARS']:
+        return "Некорректный год", 400
+
+    return f"Календарь за {selected_year} год успешно загружен!"
